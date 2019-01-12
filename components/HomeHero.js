7c5cf019571e6691b3d4bd4ae5dce700102c941d
Diego Bernal <diego91bernal@gmail.com>
2019-01-11 17:00:07 -0800

    feature: responsive navbar

--- components/HomeHero.js
+++ components/HomeHero.js
@@ -11,11 +11,15 @@ export default function HomeHero() {
             </div>
             <div className="hero-body">
                 <div className="container has-text-centered d-flex flex-column align-items-center">
-                    <figure className="avatar-figure image is-384x384">
+                    <figure className="avatar-figure image is-384x384 animated fadeInDown">
                         <img className="is-rounded border border-white avatar" src={Avatar} />
                     </figure>
-                    <h1 className="title is-1 has-text-grey-darker pt-2">Isabel Quintero</h1>
-                    <h2 className="subtitle is-2 has-text-grey pt-2">Lorem Ipsum</h2>
+                    <h1 className="title is-1 has-text-grey-darker pt-2 animated fadeInUp">
+                        Isabel Quintero
+                    </h1>
+                    <h2 className="subtitle is-2 has-text-grey pt-2 animated fadeInUp">
+                        Lorem Ipsum
+                    </h2>
                 </div>
             </div>
         </section>
--- components/Nav.js
+++ components/Nav.js
@@ -1,56 +1,73 @@
 import Link from "next/link";
 
-export default function Nav() {
-    return (
-        <nav className="navbar has-background-white" role="navigation" aria-label="main navigation">
-            <div className="navbar-brand">
-                <Link href="/">
-                    <a className="navbar-item has-text-weight-bold has-background-link has-text-white">
-                        IQ
-                    </a>
-                </Link>
-                <Link href="">
-                    <a className="navbar-item">
-                        <i className="fab fa-facebook-f" />
-                    </a>
-                </Link>
-                <Link href="">
-                    <a className="navbar-item">
-                        <i className="fab fa-twitter" />
-                    </a>
-                </Link>
-                <Link href="">
-                    <a className="navbar-item">
-                        <i className="fab fa-instagram" />
-                    </a>
-                </Link>
-                <a
-                    role="button"
-                    className="navbar-burger burger"
-                    aria-label="menu"
-                    aria-expanded="false"
-                    data-target="navbarItemsWrapper">
-                    <span aria-hidden="true" />
-                    <span aria-hidden="true" />
-                    <span aria-hidden="true" />
-                </a>
-            </div>
+export default class Nav extends React.Component {
+    state = {
+        isNavbarOpen: false
+    };
 
-            <div id="navbarItemsWrapper" className="navbar-menu">
-                <div className="navbar-start" />
+    handleToggleNavbar = () => {
+        const {isNavbarOpen} = this.state;
+        this.setState({isNavbarOpen: !isNavbarOpen});
+    };
 
-                <div className="navbar-end">
+    render() {
+        const {isNavbarOpen} = this.state;
+        return (
+            <nav
+                className="navbar has-background-white"
+                role="navigation"
+                aria-label="main navigation">
+                <div className="navbar-brand">
                     <Link href="/">
-                        <a className="navbar-item">Home</a>
+                        <a className="navbar-item has-text-weight-bold has-background-link has-text-white">
+                            IQ
+                        </a>
                     </Link>
-                    <Link href="/work">
-                        <a className="navbar-item">Work</a>
+                    <Link href="">
+                        <a className="navbar-item">
+                            <i className="fab fa-facebook-f" />
+                        </a>
                     </Link>
-                    <Link href="/events">
-                        <a className="navbar-item">Events</a>
+                    <Link href="">
+                        <a className="navbar-item">
+                            <i className="fab fa-twitter" />
+                        </a>
                     </Link>
+                    <Link href="">
+                        <a className="navbar-item">
+                            <i className="fab fa-instagram" />
+                        </a>
+                    </Link>
+                    <a
+                        onClick={this.handleToggleNavbar}
+                        role="button"
+                        className={`navbar-burger burger ${isNavbarOpen ? "is-active" : ""}`}
+                        aria-label="menu"
+                        aria-expanded="false"
+                        data-target="navbarItemsWrapper">
+                        <span aria-hidden="true" />
+                        <span aria-hidden="true" />
+                        <span aria-hidden="true" />
+                    </a>
                 </div>
-            </div>
-        </nav>
-    );
+
+                <div
+                    id="navbarItemsWrapper"
+                    className={`navbar-menu ${isNavbarOpen ? "is-active" : ""}`}>
+                    <div className="navbar-start" />
+                    <div className="navbar-end">
+                        <Link href="/">
+                            <a className="navbar-item">Home</a>
+                        </Link>
+                        <Link href="/work">
+                            <a className="navbar-item">Work</a>
+                        </Link>
+                        <Link href="/events">
+                            <a className="navbar-item">Events</a>
+                        </Link>
+                    </div>
+                </div>
+            </nav>
+        );
+    }
 }
