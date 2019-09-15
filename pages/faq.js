b4b5daa93f0b5d923e56728999e30e7c2bfc3936
Diego Bernal <diego91bernal@gmail.com>
2019-09-15 14:22:46 -0700

    feature: faq page + cms

--- pages/faq.js
+++ pages/faq.js
@@ -1,5 +1,62 @@
-import React from "react";
+//import React from "react";
+import MainLayout from "../containers/MainLayout";
+import Head from "../components/Head";
+import Section from "../components/Section";
+import AttentionBanner from "../components/AttentionBanner";
+import PageTitleHeading from "../components/PageTitleHeading";
+import styled from "styled-components";
 
-export default function FAQPage() {
-    return <h1>FAQ</h1>;
+const QAContainer = styled.div`
+    display: flex;
+
+    padding: 1rem;
+
+    span {
+        padding-right: 1rem;
+        line-height: 1;
+    }
+`;
+
+export default function FAQPage({data, metadata}) {
+    const {title, description, faq} = data;
+
+    return (
+        <MainLayout metadata={metadata}>
+            <Head description={description} title={`${title} | La Isabel Quintero`} />
+            <AttentionBanner>
+                <PageTitleHeading title={title} />
+            </AttentionBanner>
+            <Section>
+                {faq.map(item => {
+                    return (
+                        <div className="box is-paddingless">
+                            <QAContainer className="has-background-light">
+                                <span className="is-size-1 q">Q.</span>
+                                <p>{item.question}</p>
+                            </QAContainer>
+                            <QAContainer>
+                                <span className="is-size-1 is-size-5-mobile has-text-primary">
+                                    A.
+                                </span>
+                                <p>{item.answer}</p>
+                            </QAContainer>
+                        </div>
+                    );
+                })}
+            </Section>
+        </MainLayout>
+    );
 }
+
+FAQPage.getInitialProps = async () => {
+    const dataAsync = import("../_data/_pages/_faq.json");
+    const metadataAsync = import("../_data/_metadata.json");
+
+    const data = await dataAsync;
+    const metadata = await metadataAsync;
+
+    return {
+        data: data.default,
+        metadata: metadata.default
+    };
+};
